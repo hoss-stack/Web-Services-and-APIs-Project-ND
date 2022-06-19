58191c943f021e7b0380fdeae6afb15b19bfb90e
Hossam Reda <hossam.r.hassan@gmail.com>
2022-06-19 04:07:49 +0200

    Convert pricing-service REST API to MiroService

--- P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,34 @@
 package com.udacity.pricing;
 
+import com.udacity.pricing.domain.price.Price;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.Assert;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class PricingServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @LocalServerPort
+    private int port;
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void getPrice() {
+        ResponseEntity<Price> response =
+                    this.restTemplate.getForEntity("http://localhost:" + port + "/services/price?vehicleId=1", Price.class);
+
+        Assert.assertEquals(response.getStatusCode(), HttpStatus.OK);
+    }
 }
--- README.md
+++ README.md
@@ -1,9 +1,16 @@
-## About this Repository
-This repository is related to the Java Web Developer (ND035), Course - **Web Services and APIs**
+# ND035-P02-VehiclesAPI-Project
 
-It contains the following folders:
-1. Exercise-Lesson2: Contains the classroom exercise
-2. P02-VehiclesAPI: This folder contains a project readme file that has the instructions to follow
+Project repository for JavaND Project 2, where students implement a Vehicles API using Java and Spring Boot that can communicate with separate location and pricing services.
 
-## License
-[License](LICENSE.txt)
+## Instructions
+
+Check each component to see its details and instructions. Note that all three applications
+should be running at once for full operation. Further instructions are available in the classroom.
+
+- [Vehicles API](P02-VehiclesAPI/vehicles-api/README.md)
+- [Pricing Service](P02-VehiclesAPI/pricing-service/README.md)
+- [Boogle Maps](P02-VehiclesAPI/boogle-maps/README.md)
+
+## Dependencies
+
+The project requires the use of Maven and Spring Boot, along with Java v11.
